a34c51f76078f9dde3f60f02c51cc50ea81c11bd
RAZIULLAH-KHAN12 <raziullah.k@prominentpixel>
2023-12-05 12:02:24 +0530

    In exercise Fix Qs1 and Qs2

--- Exercise/Q1.ts
+++ Exercise/Q1.ts
@@ -1,16 +1,38 @@
 /*1) Print the number after the seconds using promise(also number is value of second), i.e arr = [2,4,1,3]  Print 2 after 2 sec, than print 4 after next 4 sec,...   */
 
+// const arr: number[] = [2,4,1,3];
+// var delay: number = 1000;
+
+// for(let i = 0; i<arr.length; i++) {
+//     const promise = new Promise<number>((resolve,reject) => {
+//         setTimeout(() => {
+//             return resolve(arr[i]);
+//         },delay*arr[i]);
+//     });
+//     promise.then((data) => {
+//         console.log(data);
+//     }).catch((er) => {
+//         console.log(er);
+//     })
+// }
+
+
+//This is updated code print 2 after 2 second, print 4 after 6 second, print 1 after 7 second and so on...
+
 const arr: number[] = [2,4,1,3];
 var delay: number = 1000;
+// var prevTime: number = 0;
 
 for(let i = 0; i<arr.length; i++) {
     const promise = new Promise<number>((resolve,reject) => {
         setTimeout(() => {
             return resolve(arr[i]);
-        },delay*arr[i]);
+        },delay);
+        delay += 1000 * arr[i];    
     });
     promise.then((data) => {
         console.log(data);
+        // delay += 1000 * arr[i]
     }).catch((er) => {
         console.log(er);
     })
--- Exercise/Q2.ts
+++ Exercise/Q2.ts
@@ -1,37 +1,43 @@
 /*2) From the array of 10 promise, if any 2 promise are Rejected then output should be combination of the error message of both ,the promise after all the promise are resolved(Prmoise should be executed one after one).  */
 
-let arrPromise: Promise<any>[] = [];
-let errorMessages: string[] = [];
+let errorMessages: PromiseRejectedResult[] = [];
+const arrPromise: Promise<any>[] = [];
 
-for (let i = 0; i < 10; i++) {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (i < 2) {
-        errorMessages.push("Error in Promise " + i);
-        reject("Error in Promise " + i);
-      } else {
-        resolve("Promise " + i);
-      }
-    }, 1000 * i); 
-  });
-  arrPromise.push(promise);
-}
-
-let rejectedCount = 0;
+const promise1 = new Promise((res, rej) => res("success1"));
+arrPromise.push(promise1);
+const promise2 = new Promise((res, rej) => res("success2"));
+arrPromise.push(promise2);
+const promise3 = new Promise((res, rej) => res("success3"));
+arrPromise.push(promise3);
+const promise4 = new Promise((res, rej) => res("success4"));
+arrPromise.push(promise4);
+const promise5 = new Promise((res, rej) => rej("error5"));
+arrPromise.push(promise5);
+const promise6 = new Promise((res, rej) => res("success6"));
+arrPromise.push(promise6);
+const promise7 = new Promise((res, rej) => res("success7"));
+arrPromise.push(promise7);
+const promise8 = new Promise((res, rej) => res("success8"));
+arrPromise.push(promise8);
+const promise9 = new Promise((res, rej) => rej("error9"));
+arrPromise.push(promise9);
+const promise10 = new Promise((res, rej) => res("success10"));
+arrPromise.push(promise10);
+console.log(promise9);
 
 Promise.allSettled(arrPromise)
   .then((results) => {
     for (const result of results) {
       if (result.status === 'rejected') {
-        rejectedCount++;
-        if (rejectedCount === 2) {
+        errorMessages.push(result.reason);
+        if (errorMessages.length === 2) {
           console.log(errorMessages.join(', '));
           break;
         }
       }
     }
 
-    if (rejectedCount < 2) {
+    if (errorMessages.length == 0) {
       console.log("All promises resolved successfully.");
     }
   });
